d7c9ee9d824b1b5c95693102e6de1e2c5b9668b4
Bombus10 <xblythe@harmonyschool.org>
2025-10-28 14:22:25 -0400

    Refactor installation script to use whiptail for package selection and streamline package management

--- CompSci_setup.sh
+++ CompSci_setup.sh
@@ -1,126 +1,102 @@
 #!/usr/bin/env bash
 
-
-if ! find /etc/apt/sources.list.d/ -name 'vscode.sources'; then
-    echo 'Adding Visual Studio Code repository'
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/packages.microsoft.gpg 
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-else 
-    echo 'visual studio code repo already installed, skipping'
-fi
-
-echo 'updating your system'
-sudo apt -y update && sudo apt -y upgrade
-
-echo 'updating snap packages'
-sudo snap refresh
-
-if ! snap list | grep -q '^opera'; then
-    echo 'installing opera'
-    sudo snap install opera
-else
-    echo 'opera already installed, skipping'
-fi
-
-echo 'enabling firewall'
-sudo ufw enable
-
-if ! dpkg -l | grep -q 'ii plocate'; then
-    echo 'installing plocate'
-    sudo apt -y install plocate
-else 
-    echo 'plocate already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii smartmontools'; then
-    echo 'installing smartmontools'
-    sudo apt -y install smartmontools
-else 
-    echo 'smartmontools already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii code'; then
-    echo 'installing VSCode'
-    sudo apt -y install code
-else 
-    echo 'VSCode already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii shellcheck'; then
-    echo 'installing shellcheck'
-    sudo apt -y install shellcheck
-else 
-    echo 'shellcheck already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii glmark2'; then
-    echo 'installing glmark2'
-    sudo apt -y install glmark2
-else 
-    echo 'glmark2 already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii net-tools'; then
-    echo 'installing net-tools'
-    sudo apt -y install net-tools
-else 
-    echo 'net-tools already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii  gnome-shell-extension-manager'; then
-    echo 'installing gnome shell extension manager'
-    sudo apt -y install gnome-shell-extension-manager
-else 
-    echo 'gnome shell extension manager already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii  curl'; then
-    echo 'installing curl'
-    sudo apt -y install curl
-else 
-    echo 'curl already installed, skipping'
-fi
-
-if ! dpkg -l | grep -q 'ii  source-highlight'; then
-    echo 'enabling source highlight for less'
-    sudo apt -y install source-highlight
-else 
-    echo 'source-highlight already installed, skipping'
-fi
-
-if grep -q 'export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"' ~/.bashrc && grep -q "export LESS=' -cR '" ~/.bashrc; then
-    echo 'less already configured to use source highlight, skipping'
-else
-    echo 'configuring less to use source highlight'
-    echo -e '\n # enable source highlighting in less' >> ~/.bashrc
-    echo 'export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"' >> ~/.bashrc
-    echo "export LESS=' -cR '" >> ~/.bashrc
-    source ~/.bashrc
-fi
-
-echo 'setting up system preferences'
-gsettings set org.gnome.shell favorite-apps "['opera_opera.desktop', 'firefox_firefox.desktop, 'org.gnome.Terminal.desktop', 'org.gnome.Settings.desktop', 'code.desktop', 'org.gnome.Nautilus.desktop']"
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
-
-cur_git_user=$(git config --global user.name)
-
-if [ -z "$cur_git_user" ]; then
-    echo 'Configuring github'
-    read -p "Enter Github Username: " githubusername
-    read -p "Enter Github Email: " githubemail
-    git config --global user.name "$githubusername"
-    git config --global user.email "$githubemail"
-else
-    echo "Github already configured with username ${cur_git_user}, skipping"
-fi
+PACKAGE_LIST=$(whiptail --fb --separate-output --checklist "Install Packages" 30 100 20\
+    "opera" "Opera browser (snap package)" ON \
+    "code" "Visual Studio Code (snap package)" OFF \
+    "smartmontools" "Control and monitor storage systems using S.M.A.R.T." ON \
+    "shellcheck" "Lint tool for shell scripts. Used by VSCode" ON \
+    "glmark2-wayland" "Graphics card benchmark for Wayland" ON \
+    "net-tools" "Networking toolkit: ifconfig, nslookup, ping" ON \
+    "gnome-shell-extension-manager" "Used for installing custom-hot-corners" ON \
+    "curl" "Command line tool for transferring data with URL syntax " ON \
+    "ufw" "Enable firewall" ON \
+    "source-highlight" "Allows syntax highlighting in less" ON \
+    3>&1 1>&2 2>&3)
 
 
-echo 'cleaning up'
+if [[ -n "$PACKAGE_LIST" ]]; then
+# Update all packages
+    sudo apt -y update && sudo apt -y upgrade
+    sudo snap refresh
+    
+    for select_package in $PACKAGE_LIST; do
+        case "$select_package" in
+            # Install Snap Packages
+            opera|code)
+                echo -e "\n#### Installing $select_package (snap package) ####"
+                sudo snap install "$select_package"
+                ;;
+            # Install Apt Packages
+            *)
+                echo -e "\n#### Installing $select_package ####"
+                sudo apt -y install "$select_package"
+                # Special configurations
+                case "$select_package" in
+                    source-highlight)
+                        if ! grep -q 'export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"' ~/.bashrc && grep -q "export LESS=' -cR '" ~/.bashrc; then
+                        cat << 'EOF' >> ~/.bashrc
+                        
+                        # enable source highlighting in less
+                        export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+                        export LESS=' -cR '
+EOF
+                        # shellcheck disable=SC1090
+                        source ~/.bashrc
+                        fi
+                        ;;
+                        git)
+                            cur_git_user=$(git config --global user.name)
+                            if [ -z "$cur_git_user" ]; then
+                                echo 'Configuring github'
+                                read -p "Enter Github Username: " githubusername
+                                read -p "Enter Github Email: " githubemail
+                                git config --global user.name "$githubusername"
+                                git config --global user.email "$githubemail"
+                            else
+                                echo "Github already configured with username ${cur_git_user}"
+                            fi
+                        ;;
+                        ufw)
+                            sudo ufw enable
+                        ;;
+                esac
+        esac
+    done
 sudo apt -y autoremove
+fi
+
+
+TWEAK_LIST=$(whiptail --fb --separate-output --checklist "User Interface Tweaks" 30 100 20 \
+    "Pin-Apps" "Pin favorite-apps to dock: Opera, Firefox, Terminal, Settings, VSCode, Files" ON \
+    "smartmontools" "Control and monitor storage systems using S.M.A.R.T." ON \
+    "Dark-Mode" "Set color scheme to prefer-dark" ON \
+    "Hide-Dock" "Hide dock when not in use" ON \
+    "Multimonitor-Dock" "Show dock on all monitors" ON \
+    "Dock-Bottom" "Position dock to bottom of screen" ON \
+    3>&1 1>&2 2>&3)
+
+if [[ -n "$TWEAK_LIST" ]]; then
+    for select_tweak in $TWEAK_LIST; do
+        case "$select_tweak" in
+            "Pin-Apps")
+            gsettings set org.gnome.shell favorite-apps "['opera_opera.desktop', 'firefox_firefox.desktop, 'org.gnome.Terminal.desktop', 'org.gnome.Settings.desktop', 'code.desktop', 'org.gnome.Nautilus.desktop']"
+            ;;
+            "Dark-Mode")
+            gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+            ;;
+            "Hide-Dock")
+            gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+            gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+            gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
+            ;;
+            "Dock-Bottom")
+            gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+            ;;
+            "Multimonitor-Dock")   
+            gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
+            ;;
+        esac
+    done
+fi
 
 echo "thank you for using Xander's update CompSci script!" 
